1c88a96459decea564c05166b963174634b58f76
Donny <1021001390@qq.com>
2017-04-30 11:17:29 +0800

    add start serve and build method interface.

--- app.js
+++ app.js
@@ -4,17 +4,32 @@ var shell = require('shelljs');
 var argv = require('yargs').argv;
 var Git = require('nodegit');
 var repository = "https://github.com/Donny2333/angular-seed.git";
-var local = process.argv || 'dufu';
+var method = process.argv[2];
+var local = process.argv[3] || 'dufu';
 
 if (!shell.which('git')) {
     shell.echo('Sorry, this script requires git');
     shell.exit(1);
 }
 
-shell.echo('Start Clone.');
+switch (method) {
+    case 'start':
+        Git.Clone(repository, local).then(function (repository) {
+            shell.echo(local + ' has done!');
+        }, function (err) {
+            shell.echo(err);
+        });
+        break;
 
-Git.Clone(repository, local).then(function(repository) {
-    shell.echo(local + ' has done!');
-}, function(err) {
-    shell.echo(err);
-});
+    case 'serve':
+        // Todo: launch the web project
+        break;
+
+    case 'build':
+        // Todo: build the project
+        break;
+
+    default:
+        break;
+}
+
